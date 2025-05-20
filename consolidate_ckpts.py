e9b7200c309b98a311ac190ce69522b30099b2d7
jwkirchenbauer <jkirchen@umd.edu>
2025-05-19 22:23:37 -0700

    testing convert and eval automations, kicked phase3

--- consolidate_ckpts.py
+++ consolidate_ckpts.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import re
 import os
 
+import argparse
+
 # import torch
 
 from lingua.args import dataclass_from_dict
@@ -17,73 +19,92 @@ from apps.main.eval import (
 from lingua.checkpoint import RE_FOLDER, _get_key_step
 
 
-args_eval = {
-    "harness": {
-        "tasks": [
-            "hellaswag",
-            {"task": "boolq", "dataset_kwargs": {"trust_remote_code": True}},
-            "piqa",
-            {"task": "social_iqa", "dataset_kwargs": {"trust_remote_code": True}},
-            "winogrande",
-            "openbookqa",
-            "arc_easy",
-            "arc_challenge",
-            "race",
-            "commonsense_qa",
-            {"task": "copa", "dataset_kwargs": {"trust_remote_code": True}},
-            "mmlu",
-            "mmlu_pro",
-        ],
-    },
-    "generator": {
-        "max_tokens": 8192,
-        "dtype": "bf16",
-    },
-}
+def run_conversion(run_dump_dir, run_ckpts_dir, train_state_step):
+    args_eval = {
+        "harness": {
+            "tasks": [
+                "hellaswag",
+                {"task": "boolq", "dataset_kwargs": {"trust_remote_code": True}},
+                "piqa",
+                {"task": "social_iqa", "dataset_kwargs": {"trust_remote_code": True}},
+                "winogrande",
+                "openbookqa",
+                "arc_easy",
+                "arc_challenge",
+                "race",
+                "commonsense_qa",
+                {"task": "copa", "dataset_kwargs": {"trust_remote_code": True}},
+                "mmlu",
+                "mmlu_pro",
+            ],
+        },
+        "generator": {
+            "max_tokens": 8192,
+            "dtype": "bf16",
+        },
+    }
 
+    def get_existing_saves(ckpt_dir):
+        folders = [
+            p
+            for p in Path(ckpt_dir).iterdir()
+            if p.is_dir() and re.match(RE_FOLDER, p.name)
+        ]
+        folders.sort(key=lambda p: _get_key_step(p.name))
+        return folders
 
-def get_existing_saves(ckpt_dir):
-    folders = [
-        p
-        for p in Path(ckpt_dir).iterdir()
-        if p.is_dir() and re.match(RE_FOLDER, p.name)
-    ]
-    folders.sort(key=lambda p: _get_key_step(p.name))
-    return folders
+    eval_args = dataclass_from_dict(EvalArgs, args_eval)
 
+    eval_args.global_step = train_state_step
 
-RUN_DUMP_DIR = f"/p/lustre5/kirchenb/common-pile-root/lingua/output/prod_lingua_64N"
-RUN_CKPTS_DIR = f"{RUN_DUMP_DIR}/checkpoints"
-TRAIN_STATE_STEP = 10000
+    # find the dir that matches the step
+    all_saves = get_existing_saves(run_ckpts_dir)
 
+    for save in all_saves:
+        if _get_key_step(save.name) == train_state_step:
+            eval_args.ckpt_dir = str(save)
+            break
+    else:
+        raise ValueError(
+            f"Could not find checkpoint for step {train_state_step} in {run_ckpts_dir}"
+        )
 
-eval_args = dataclass_from_dict(EvalArgs, args_eval)
-
-eval_args.global_step = TRAIN_STATE_STEP
-
-# find the dir that matches the step
-all_saves = get_existing_saves(RUN_CKPTS_DIR)
-for save in all_saves:
-    if _get_key_step(save.name) == TRAIN_STATE_STEP:
-        eval_args.ckpt_dir = str(save)
-        break
-else:
-    raise ValueError(
-        f"Could not find checkpoint for step {TRAIN_STATE_STEP} in {RUN_CKPTS_DIR}"
+    eval_args.dump_dir = str(
+        os.path.join(
+            run_dump_dir,
+            "evals",
+            EVAL_FOLDER_NAME.format(train_state_step),
+        )
     )
+    eval_args.metric_log_dir = run_dump_dir
+
+    launch_consolidate_only(eval_args)
 
 
-eval_args.dump_dir = str(
-    os.path.join(
-        RUN_DUMP_DIR,
-        "evals",
-        EVAL_FOLDER_NAME.format(TRAIN_STATE_STEP),
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--run_dump_dir",
+        type=str,
+        default="/p/lustre5/kirchenb/common-pile-root/lingua/output/prod_lingua_64N",
     )
-)
-eval_args.metric_log_dir = RUN_DUMP_DIR
+    parser.add_argument(
+        "--run_ckpts_subdir",
+        type=str,
+        default="checkpoints",
+    )
+    parser.add_argument(
+        "--train_state_step",
+        type=int,
+        default=10000,
+    )
+    args = parser.parse_args()
 
-launch_consolidate_only(eval_args)
-
+    run_conversion(
+        run_dump_dir=args.run_dump_dir,
+        run_ckpts_dir=f"{args.run_dump_dir}/{args.run_ckpts_subdir}",
+        train_state_step=args.train_state_step,
+    )
 
 # ls -la /p/vast1/pretrain/models/comma-v0.1/checkpoints/0000100000/consolidated/
 # total 27354220
@@ -101,7 +122,14 @@ launch_consolidate_only(eval_args)
 # && \
 # cp /p/vast1/pretrain/models/comma-v0.1-checkpoint-hf/*tok*.json /p/lustre5/kirchenb/common-pile-root/lingua/output/debug_conversion/hf_models/comma_v0-1_100000
 
+# olmes --model /p/lustre5/kirchenb/common-pile-root/lingua/output/debug_conversion/hf_models/comma_v0-1_100000 --model-args '{"add_bos_token": true}' --task ablation:knowledge::comma --output-dir /p/lustre5/kirchenb/common-pile-root/lingua/output/debug_conversion/evals
+
 # the additional rename and a one liner to extract the model from the state dict allows the conversion to complete
+
+# python consolidate_ckpts.py \
+#     --run_dump_dir /p/lustre5/kirchenb/common-pile-root/lingua/output/prod_lingua_64N \
+#     --run_ckpts_subdir checkpoints \
+#     --train_state_step 38000
 
 # mv /p/lustre5/kirchenb/common-pile-root/lingua/output/debug_conversion/prod_lingua_64N_ckpts_10000/consolidated/consolidated.pth \
 # /p/lustre5/kirchenb/common-pile-root/lingua/output/debug_conversion/prod_lingua_64N_ckpts_10000/consolidated/model.pth \
---
+++ launch_convert_and_eval.py
@@ -0,0 +1,193 @@
+import os
+import glob
+import re
+import json
+from itertools import product, chain
+import subprocess
+
+DRY_RUN = True
+# DRY_RUN = False
+
+WRITE_ONLY = False
+# WRITE_ONLY = True
+
+DO_CONVERT = True
+DO_EVAL = False
+
+# DO_CONVERT = False
+# DO_EVAL = True
+
+assert DO_CONVERT ^ DO_EVAL, "You must choose one of DO_CONVERT or DO_EVAL"
+
+# EVAL_NODE_COUNT = None
+EVAL_NODE_COUNT = 1
+
+# EVAL_GPUS_PER_NODE = None
+EVAL_GPUS_PER_NODE = 1
+
+LAUNCHER_FILEPATH = "/p/lustre5/$USER/llnl-tools/launch_tuo.py"
+
+RCCL_INSTALL_DIR = "/collab/usr/global/tools/rccl/$SYS_TYPE/rocm-6.3.1/install/lib"
+
+# CONVERSION_CONDA_ENV = "/usr/workspace/wsb/kirchenb/tuolumne_conda_28_630_lingua"
+# EVAL_CONDA_ENV = "/usr/workspace/wsb/kirchenb/tuolumne_conda_28_630_olmes"
+
+BASHRC_FILE = "/g/g11/kirchenb/.bashrc"
+
+WRKSPC = os.getenv("WRKSPC")
+
+# WANDB_PROJECT_NAME = "common"
+# WANDB_OFFLINE = "False"
+# WANDB_OFFLINE = "True"
+
+BASE_OUT_DIR = f"/p/lustre5/kirchenb/common-pile-root/lingua/output"
+
+BASE_RUN_NAME = "prod_lingua_64N_convert_eval"
+
+# QOS = "pdebug"
+QOS = "pbatch"
+
+ROCM_VERSION = "6.3.0"
+RCCL_MODE = "rdzv-lbann"
+
+JOB_LIMIT = None
+# JOB_LIMIT = 1
+
+# TIME_LIMIT = 59
+# TIME_LIMIT = 119
+TIME_LIMIT = 480
+
+TAG_LIST = [
+    "eval",
+]
+TAG_LIST = [str(t).replace(".", "") for t in TAG_LIST]
+WANDB_TAG_STRING = f"[{','.join(TAG_LIST)}]"
+
+# models
+exp_list = [
+    [
+        "prod_lingua_64N",
+    ],
+]
+
+# STEPS = [10_000]
+# STEPS = [20_000, 30_000]
+# STEPS = [s for s in range(10_000, 40_001, 10_000)] + [38_000, 42_000]
+STEPS = [s for s in range(20_000, 40_001, 10_000)] + [38_000, 42_000]
+
+hparam = [
+    # "ablation:knowledge::comma",
+    "ablation:all::comma",
+]
+final_exp_list = list(chain(*[[exp + [hp] for hp in hparam] for exp in exp_list]))
+# final_exp_list = exp_list
+
+if JOB_LIMIT is not None:
+    final_exp_list = final_exp_list[:JOB_LIMIT]
+
+for exp in final_exp_list:
+    print(exp)
+
+total_launches = 0
+
+# queue all jobs
+for exp in final_exp_list:
+
+    (
+        run_name,
+        eval_task_spec,
+    ) = exp
+
+    custom_invocation = ""
+
+    parent_dir = f"{BASE_OUT_DIR}/{run_name}"
+    run_ckpts_subdir = "checkpoints"
+    checkpoint_dir = f"{parent_dir}/{run_ckpts_subdir}"
+    hf_checkpoint_dir = f"{parent_dir}/checkpoints_hf"
+    evals_dir = f"{parent_dir}/olmes_evals"
+
+    if not os.path.exists(parent_dir):
+        continue
+
+    # step_numbers = [step for step in STEPS]
+    step_numbers = [step for step in sorted(STEPS)]
+    step_numbers_10digit = [f"{int(step):010d}" for step in step_numbers]
+    print(step_numbers)
+    print(step_numbers_10digit)
+
+    for step_number, step_number_10digit in zip(step_numbers, step_numbers_10digit):
+
+        # NOTE: WARNING no gaurds here if already done
+
+        # if custom_invocation != "":
+        #     custom_invocation += "\n\n"
+        if DO_CONVERT:
+            # custom_invocation += f"""\
+            custom_invocation = f"""\
+echo "Converting checkpoint {step_number} for {run_name}"
+
+python consolidate_ckpts.py \
+--run_dump_dir {parent_dir} \
+--run_ckpts_subdir checkpoints \
+--train_state_step {step_number}
+
+mv {checkpoint_dir}/{step_number_10digit}/consolidated/consolidated.pth \
+{checkpoint_dir}/{step_number_10digit}/consolidated/model.pth
+
+mkdir -p {hf_checkpoint_dir}/{step_number_10digit}
+
+python convert_lingua_weights_to_hf.py \
+--input_dir {checkpoint_dir}/{step_number_10digit}/consolidated \
+--output_dir {hf_checkpoint_dir}/{step_number_10digit} \
+--llama_version 2 \
+--num_shards 1 \
+--model_size 7B
+
+cp /p/vast1/pretrain/models/comma-v0.1-checkpoint-hf/*tok*.json {hf_checkpoint_dir}/{step_number_10digit}
+
+echo "Done converting {step_number} for {run_name}!"
+
+"""
+        if DO_EVAL:
+            # model_args_str = r"'{" + r'"add_bos_token": true' + r"}'"
+            # had to hardcode it into the library's cli defaults :/
+            # custom_invocation += f"""\
+            custom_invocation = f"""\
+echo "Evaluating checkpoint {step_number} for {run_name}"
+
+mkdir -p {evals_dir}/{step_number_10digit}
+
+olmes --model {hf_checkpoint_dir}/{step_number_10digit} --task {eval_task_spec} --output-dir {evals_dir}/{step_number_10digit}
+
+echo "Done evaluating {step_number} for {run_name}!"
+
+"""
+
+        if DO_CONVERT or DO_EVAL:
+
+            command = f"""\
+    python {LAUNCHER_FILEPATH} \
+    --output_dir={BASE_OUT_DIR}/{BASE_RUN_NAME} \
+    --rccl_installdir={RCCL_INSTALL_DIR} \
+    --rccl_mode={RCCL_MODE} \
+    --qos={QOS} \
+    --rocm_version={ROCM_VERSION} \
+    --run_name={run_name}{'_convert' if DO_CONVERT else ''}{'_eval' if DO_EVAL else ''}_{step_number_10digit} \
+    --nodes={EVAL_NODE_COUNT} \
+    --gpus_per_node={EVAL_GPUS_PER_NODE} \
+    --minutes={TIME_LIMIT} \
+    --custom_invocation='{custom_invocation}' \
+    --pass_run_name=False \
+    {f'--dryrun' if WRITE_ONLY else ''}\
+    """
+
+            if custom_invocation == "":
+                command = f'echo "Nothing to run for {run_name}"'
+
+        total_launches += 1
+        if not DRY_RUN:
+            os.system(command)
+        else:
+            print(run_name)
+
+print(f"Total launches: {total_launches}")
